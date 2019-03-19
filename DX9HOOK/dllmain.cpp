e5394bc726c0b02d555fe3ea7fccae43f814a56e
StaleDeveloper <StaleDeveloper@gmail.com>
2019-03-19 11:44:22 +0000

    Blocker
    Implemented
    - BlockTick Function
    - File Hashing checks
    
    Program now:
    Kills process related to blocked programs.
    Logs interference

--- DX9HOOK/dllmain.cpp
+++ DX9HOOK/dllmain.cpp
@@ -18,6 +18,9 @@
 #include "Imgui/imstb_textedit.h"
 #include "Imgui/imstb_truetype.h"
 
+typedef int(*thook)();
+thook oHook;
+
 using namespace std;
 bool DX9HookSuccess = false;
 
@@ -25,20 +28,49 @@ void Hook_D3D9_Direct3DCreate9();
 void Hook_D3D9_CreateDevice();
 void Hook_D3D9_EndScene();
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
-{
-	switch (ul_reason_for_call)
-	{
-	case DLL_PROCESS_ATTACH:
-		Hook_D3D9_Direct3DCreate9();
-		break;
-	case DLL_THREAD_ATTACH:
-	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
-		break;
+void GetTargetedEnemy();
+bool doesContain(DWORD);
+void initEntitylist();
+
+int ix;
+int counter = 0;
+DWORD Addy;
+DWORD addyarray[256] = { };
+bool duplicate = false;
+
+int hHook() {
+	_asm {
+		fsub DWORD ptr [eax+0x60]
+		fstp DWORD ptr [ebp-0x08]
+		mov Addy, eax
+	}	
+
+	if (ix < 256) {
+			addyarray[ix] = Addy;
+			ix++;
 	}
-	return TRUE;
+
+	return oHook();
 }
+
+//bool doesContain(DWORD checkaddy) {
+//	//bool contains = false;
+//	//Foo array[10];
+//	//	Foo *foo = std::find(std::begin(array), std::end(array), someObject);
+//	//// When the element is not found, std::find returns the end of the range
+//	//if (foo != std::end(array)) {
+//	//	cerr << "Found at position " << std::distance(array, foo) << endl;
+//	//}
+//	//else {
+//	//	cerr << "Not found" << endl;
+//	//}
+//	//for (int x = 0; x <= ix; x++) {
+//	//	if (addyarray[x] == checkaddy) {
+//	//		contains = true;
+//	//	}
+//	//}
+//	return contains;
+//}
 
 bool DetourHookFunction(PVOID *ppPointer, PVOID pDetour)
 {
@@ -50,6 +82,37 @@ bool DetourHookFunction(PVOID *ppPointer, PVOID pDetour)
 		return false;
 	return true;
 }
+
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		AllocConsole(); // enables the console
+		freopen("CONOUT$", "w", stdout);
+		initEntitylist();
+		oHook = (thook)(0x00F51555);
+		DetourHookFunction(&(PVOID&)oHook, hHook);
+		Hook_D3D9_Direct3DCreate9();
+		break;
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
+void initEntitylist() {
+	for (int x = 0; x < 256; x++) {
+		addyarray[x] = 0x0;
+	}
+}
+
+
+		//DWORD fbase = (DWORD)GetModuleHandleA("Fiesta.exe");
+		//DWORD AddyToHook = fbase + 0x251BB9;
 
 // Hook D3D9 Direct3DCreate9
 void Hook_D3D9_Direct3DCreate9()
@@ -103,6 +166,16 @@ void Hook_D3D9_EndScene()
 	DetourHookFunction(&(PVOID&)EndScene_Original, EndScene_Hook);
 }
 
+
+DWORD_PTR Base;
+DWORD_PTR Offset2;
+DWORD_PTR Offset3;
+DWORD_PTR Offset4;
+DWORD_PTR Offset5;
+DWORD_PTR FinalAddress;
+
+bool checkbox;
+const char* text;
 HRESULT WINAPI EndScene_Hook(IDirect3DDevice9* pDevice)
 {
 	if (GetAsyncKeyState(VK_LBUTTON) & 1 << 15) {
@@ -127,10 +200,33 @@ HRESULT WINAPI EndScene_Hook(IDirect3DDevice9* pDevice)
 	ImGui_ImplWin32_NewFrame();
 	ImGui::NewFrame();
 
-	ImGui::Text("Demo Text");
+	if (ImGui::Checkbox("GetTargetedEnemy:", &checkbox)) {
+		system("CLS");
+		for (int x = 0; x < 255; x++)
+		{
+			float xpos = *(float*)((DWORD)addyarray[x] +0x58);
+	/*		printf("eax[%d] = %X \n", x, addyarray[x]);*/
+			printf("xaddy[%d] = %f \n", x, xpos);
+		}
+	}
+
+	if (checkbox) {
+		float x = *(float*)((DWORD)Addy + 0x58);
+		float y = *(float*)((DWORD)Addy + 0x5C);
+		float z = *(float*)((DWORD)Addy + 0x60);
+		//
+		ImGui::Text("Position: (%f,%f,%f) : Count: %d : eax: %X", x,y,z,ix,Addy);
+	}
+	else {
+
+		ImGui::Text("Heya");
+	}
+
+
 
 	ImGui::EndFrame();
 	ImGui::Render();
+
 
 	ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
 
@@ -138,6 +234,17 @@ HRESULT WINAPI EndScene_Hook(IDirect3DDevice9* pDevice)
 	return hres;
 }
 
-void GetTargetedEnemy() {
 
+
+void GetTargetedEnemy() {
+	Base = (DWORD)GetModuleHandleA("Fiesta.exe");
+
+	Base = *(DWORD_PTR*)(Base + 0x006AB4A8);
+	Offset2 = *(DWORD_PTR*)(Base + 0x224);
+	Offset3 = *(DWORD_PTR*)(Offset2 + 0x138);
+	Offset4 = *(DWORD_PTR*)(Offset3 + 0x20);
+	Offset5 = *(DWORD_PTR*)(Offset4 + 0x12C);
+	FinalAddress = *(DWORD_PTR*)(Offset5 + 0x12C);
+	float HP = *(float*)((DWORD)FinalAddress);
+	text = ("HP: ");
 }
